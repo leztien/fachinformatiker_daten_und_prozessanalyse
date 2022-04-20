86392ea1aa2bfb117ddf692275acc4820c661856
I.N <48551497+leztien@users.noreply.github.com>
2022-04-20 17:24:04 +0200

    Create while_else.py

---
+++ while_else.py
@@ -0,0 +1,7 @@
+A = {1,2}
+B = {2,3,4}
+
+while A < B:  # if A is a proper subset of B
+    print("A is a proper subset of B")  # will not be implemented
+else:
+    print("else")                       # will be implemented
